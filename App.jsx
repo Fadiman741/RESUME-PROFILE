b968e76b768adf977d61acb3713bf36c1274a9ff
Mafadi Lephaka <63578113+Fadiman741@users.noreply.github.com>
2022-01-15 12:41:08 +0000

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+				<div className="containerr" >
+						
+				</div>
+
+		</div>
+  );
+}
+
+export default App;
---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+		<body>
+    	<div className="container">
+				<div className="containerr" >
+				
+						
+				</div>
+
+			</div>
+		</body>
+  );
+}
+
+export default App;
