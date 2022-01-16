1013ca969640eb99e44b7697ac49f37eb79e1a77
Mafadi Lephaka <63578113+Fadiman741@users.noreply.github.com>
2022-01-16 12:46:07 +0000

    revised navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,55 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+	
   return (
+		
 		<body>
     	<div className="container">
-				<div className="containerr" >
-						<div className="top-profile">
+						<div className="containerr" >
+									<div className="top-profile">
+												<div class="column" >
+																												
+															<h2> LEPHAKA</h2>
+   									  				<p>Web Developer</p>													
+    												
+  											</div>
+  											<div class="column" >
+    								 					<p>Email: mafadi.lephaka21@gmail.com</p>
+															<p>Contacts: 0786394580</p>
+  											</div>
+  											<div class="column" >
+    													<p>Location: Cape Town, South Africa</p>
+  											</div>
 
-						</div>
-						<div className="topnav">
-  							<a class="active" href="#home">Home</a>
-  							<a href="#news">Resume</a>
-  							<a href="#contact">Contact</a>
-  							<a href="#about">About</a>
-						</div>
-				
-						
+									</div>
+
+								<div class="tab">
+									<button class="tablinks" onclick="openCity(event, 'London')">London</button>
+									<button class="tablinks" onclick="openCity(event, 'Paris')">Paris</button>
+									<button class="tablinks" onclick="openCity(event, 'Tokyo')">Tokyo</button>
+								</div>
+
+								
+								<div id="London" class="tabcontent">
+									<h3>London</h3>
+									<p>London is the capital city of England.</p>
+								</div>
+
+								<div id="Paris" class="tabcontent">
+									<h3>Paris</h3>
+									<p>Paris is the capital of France.</p>
+								</div>
+
+								<div id="Tokyo" class="tabcontent">
+									<h3>Tokyo</h3>
+									<p>Tokyo is the capital of Japan.</p>
+								</div>
+
+
+	
 				</div>
-
 			</div>
 		</body>
   );
