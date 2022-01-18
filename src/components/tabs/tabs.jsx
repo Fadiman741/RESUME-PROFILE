a81eb740a5817e2ea4c5899b44603523f7a77ea7
Mafadi Lephaka <63578113+Fadiman741@users.noreply.github.com>
2022-01-18 14:16:51 +0000

    I was spliting code make it into components

---
+++ src/components/tabs/tabs.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './tabs.css';
+
+function Tabs() {
+	return(
+			<div>
+									<div class="tab">
+										<button class="tablinks" onclick="openCity(event, 'London')">Home</button>
+										<button class="tablinks" onclick="openCity(event, 'Paris')">Profile</button>
+										<button class="tablinks" onclick="openCity(event, 'Tokyo')">Resume</button>
+										<button class="tablinks" onclick="openCity(event, 'Tokyo')">Portfolio</button>
+										<button class="tablinks" onclick="openCity(event, 'Tokyo')">Contact</button>
+									</div>				
+
+									<div id="London" class="tabcontent">											
+										<h3>London</h3>
+										<p>London is the capital city of England.</p>
+									</div>
+
+									<div id="Paris" class="tabcontent">
+										<h3>Paris</h3>
+										<p>Paris is the capital of France.</p>
+									</div>
+
+									<div id="Tokyo" class="tabcontent">
+										<h3>Tokyo</h3>
+										<p>Tokyo is the capital of Japan.</p>
+									</div>
+				</div>
+
+	);
+}
+export default Tabs;
---
+++ src/components/topheader/topheader.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './topheader.css';
+
+
+function topheader(){
+		return(
+			<div className="top-profile">
+						<div class="column" >														
+										<h2> LEPHAKA</h2>
+					  				<p>Web Developer</p>													   												
+						</div>
+						<div class="column" >
+					 					<p>Email: mafadi.lephaka21@gmail.com</p>
+										<p>Contacts: 0786394580</p>
+						</div>
+										<div class="column" >
+										<p>Location: Cape Town, South Africa</p>
+						</div>
+			</div>
+		);
+
+
+}
+export default topheader;
